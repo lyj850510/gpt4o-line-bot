b2ee84a4de25f0f7bc48dd942fb67b7533fc0829
unknown <lyj510@mail.hsc.edu.tw>
2025-04-04 20:17:36 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import openai
+import os
+
+app = Flask(__name__)
+
+# 設定你的金鑰
+LINE_CHANNEL_ACCESS_TOKEN = '你的 LINE Token'
+LINE_CHANNEL_SECRET = '你的 LINE Secret'
+OPENAI_API_KEY = '你的 OpenAI API Key'
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+openai.api_key = OPENAI_API_KEY
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_msg = event.message.text
+    
+    # 呼叫 GPT-4o
+    response = openai.ChatCompletion.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": user_msg}
+        ]
+    )
+
+    gpt_reply = response['choices'][0]['message']['content']
+    
+    # 回覆 LINE 使用者
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=gpt_reply)
+    )
+
+if __name__ == "__main__":
+    app.run(port=5000)
